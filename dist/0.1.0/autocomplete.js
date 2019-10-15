84f5fcec37c9c5fcfde3b1f7fa3f7a82228bed55
gigtech-ret <55818261+gigtech-ret@users.noreply.github.com>
2019-10-15 13:14:09 +0800

    1.0.0

---
+++ dist/0.1.0/autocomplete.js
@@ -0,0 +1,73 @@
+/*! 
+ โครงการ: autocomplete 
+ รุ่น: 0.1.0 */
+(function(){
+    'use strict';
+
+    var moduleName = 'tgComponentsAutocomplete';
+
+    var appDependencies = [];
+
+    angular.module(moduleName, appDependencies);
+})();
+(function(){
+    'use strict';
+
+    angular
+        .module('tgComponentsAutocomplete')
+        .directive('tgAutocomplete', autocomplete);
+
+        autocomplete.$inject = ['$http', '$interpolate', '$parse'];
+    function autocomplete($http, $interpolate, $parse){
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            scope: {
+                ngModel: '=',
+                secondParameter: '=?',
+                source: '=',
+                onSelect: '=',
+                onFocus: '=',
+                onChange: '=',
+                minLength: '='
+            },
+            controller: ['$scope', function($scope){
+
+            }],
+            link: function(scope, elem, attrs){
+                elem.autocomplete({
+                    source: scope.source,
+                    minLength: scope.minLength || 3,
+                    select: function(event, selectedItem){
+                        event.preventDefault();
+                        scope.ngModel = selectedItem.item.label;
+                        if(angular.isDefined(scope.onSelect)){
+                            if(angular.isFunction(scope.onSelect)){
+                                scope.onSelect(selectedItem.item, scope.secondParameter);
+                            }
+                        }
+                        scope.$apply();
+                    },
+                    focus: function(event, selectedItem){
+                        event.preventDefault();
+                        scope.ngModel = selectedItem.item.label;
+                        if(angular.isDefined(scope.onFocus)){
+                            if(angular.isFunction(scope.onFocus)){
+                                scope.onFocus(selectedItem.item, scope.secondParameter);
+                            }
+                        }
+                        scope.$apply();
+                    },
+                    change: function(event, selectedItem){
+                        event.preventDefault();
+                        if(angular.isDefined(scope.onFocus)){
+                            if(angular.isFunction(scope.onFocus)){
+                                scope.onFocus(selectedItem.item, scope.secondParameter);
+                            }
+                        }
+                    },
+                })
+            }
+        }
+    }
+})();
